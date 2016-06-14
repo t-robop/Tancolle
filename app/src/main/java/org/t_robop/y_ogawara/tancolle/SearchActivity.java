57c9d2b0c18d3d9390f5e75fe058b335c1ed6795
multiverse2011 <multiverse2011@gmail.com>
2016-06-14 17:22:04 +0900

    test

--- app/src/main/java/org/t_robop/y_ogawara/tancolle/SearchActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/tancolle/SearchActivity.java
@@ -2,8 +2,17 @@ package org.t_robop.y_ogawara.tancolle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 public class SearchActivity extends AppCompatActivity {
+
+
+
+    private SearchView searchView;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
