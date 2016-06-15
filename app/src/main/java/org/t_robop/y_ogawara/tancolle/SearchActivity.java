23f6783e88fdc6e77b9c73ada2c8045b7ff8cb95
multiverse2011 <multiverse2011@gmail.com>
2016-06-15 21:20:29 +0900

    Listの細かいところ直した

--- app/src/main/java/org/t_robop/y_ogawara/tancolle/SearchActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/tancolle/SearchActivity.java
@@ -2,36 +2,79 @@ package org.t_robop.y_ogawara.tancolle;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.jar.Attributes;
 
 public class SearchActivity extends Activity implements
+
         SearchView.OnQueryTextListener {
 
     private SearchView searchView;
     private ListView listView;
     private ArrayAdapter<String> adapter;
 
-
+//    //adapter作る
+//    private Context context;
+//    private List<Data> list;
+//    private LayoutInflater layoutInflater = null;
+//
+//    public NameListAdapter(Context context, List<Data> list) {
+//        super();
+//        this.context = context;
+//        this.list = list;
+//        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//    }
+//    @Override
+//    public int getCount(){
+//        return list.size();
+//    }
+//    @Override
+//    public Object getItem(int position){
+//        return list.get(position);
+//    }
+//    @Override
+//    public long getItemId(int position){
+//        return position;
+//    }
+//
+//    @Override
+//    public View getView(int position, View covertView, ViewGroup parent) {
+//        Data data = (data) getItem(position);
+//        convertView = layoutInflater.inflate(R.layout.activity_seatch_list,null);
+//        TextView tv = (TextView) convertView.findViewById(R.id.listView1);
+//        tv.setText(data.getName());
+//        return convertView;
+//    }
+//
+//    //ここまで
 
     //private String[] stateList = { "Nishimura", "にしむら", "ニシムラ", "西村", "Godiva", "ゴディバ", "ごでぃば" };
-
-    //
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +82,7 @@ public class SearchActivity extends Activity implements
         setContentView(R.layout.activity_search);
 
         //Data型の宣言
-        Data testData =new Data();
+        Data testData = new Data();
         //Data型にデータをセット
         testData.setName("西村");
         testData.setKana("にしむら");
@@ -56,29 +99,28 @@ public class SearchActivity extends Activity implements
         testData.setNotif_day(3);
         testData.setNotif_recy(3);
         //dbに書き込み
-        dbAssist.insertData(testData,this);
+        dbAssist.insertData(testData, this);
 
 
         //読み込み
 
-
         //返り値は配列
-        ArrayList<Data> dataList=  dbAssist.allSelect(this);
+        ArrayList<Data> dataList = dbAssist.allSelect(this);
         Data allData = new Data();
 
         allData = dataList.get(0);
-        List <String> tes = new ArrayList<>();
-        for (int i =0;dataList.size() > i;i++){
-            tes.add(dataList.get(i).getName());
+        List<String> name = new ArrayList<>();
+        for (int i = 0; dataList.size() > i; i++) {
+            name.add(dataList.get(i).getName());
         }
-       Log.d("",allData.getMemo());
-       Log.d("aaa",dataList.get(0).getKana());
+        Log.d("", allData.getMemo());
+        Log.d("aaa", dataList.get(0).getKana());
 
 
         searchView = (SearchView) findViewById(R.id.searchView1);
         listView = (ListView) findViewById(R.id.listView1);
         // adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,dataList);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tes);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, name);
         listView.setAdapter(adapter);
 
         //テキストフィルター(リアルタイム検索)
@@ -90,40 +132,50 @@ public class SearchActivity extends Activity implements
         searchView.setIconifiedByDefault(false);
         searchView.setOnQueryTextListener(this);
 
-        // 入力するところのヒント
+        // 入力するところに最初に書いてあるやーつ
         searchView.setQueryHint("名前を入力");
 
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
 //        setSupportActionBar(toolbar);
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        // ListView がクリックされた時に呼び出されるコールバックを登録
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                //ここに書く
+                ListView listView = (ListView) parent;
+                String item = (String) listView.getItemAtPosition(position);
+
+                //トースト
+                Toast.makeText(SearchActivity.this, "Click: " + item, Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 
-
-
-    // Called when the query text is changed by the user.
+    // 検索キーワードが入ると呼ばれる
     @Override
     public boolean onQueryTextChange(String newText) {
-        // newText the new content of the query text field
+        // 表示設定
         if (TextUtils.isEmpty(newText)) {
-            // clear text filter so that list displays all items
+            // 何も入ってないとフィルターしない
             listView.clearTextFilter();
         } else {
-            // apply filter so that list displays only
-            // matching child items
-            listView.setFilterText(newText.toString());
+            //トーストでない書き方
+            Filter filter = ((Filterable) listView.getAdapter()).getFilter();
+            filter.filter(newText.toString());
+
+            //こっちにするとトーストっぽいのがでる
+            //listView.setFilterText(newText.toString());
         }
         return true;
     }
-
-
 
     @Override
     public boolean onQueryTextSubmit(String query) {
         return false;
     }
-
-
-
 
 
 }
