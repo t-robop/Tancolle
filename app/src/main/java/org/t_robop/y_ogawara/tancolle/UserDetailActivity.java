69ad3538e674038f9fd3934524feebf22a0401ee
muramari <muramari0714@gmail.com>
2016-06-15 17:36:24 +0900

    test

--- app/src/main/java/org/t_robop/y_ogawara/tancolle/UserDetailActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/tancolle/UserDetailActivity.java
@@ -2,6 +2,7 @@ package org.t_robop.y_ogawara.tancolle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class UserDetailActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class UserDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_detail);
+        Log.d("","");
     }
 }
