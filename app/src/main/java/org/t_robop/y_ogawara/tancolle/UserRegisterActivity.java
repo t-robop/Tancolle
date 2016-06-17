ee7617ce0c6c79d80a6f53628d19fc10785f9c40
taiga <6bji2119@mail.u-tokai.ac.jp>
2016-06-17 21:58:25 +0900

    画像、カレンダーやった。 カテゴリー未完成

--- app/src/main/java/org/t_robop/y_ogawara/tancolle/UserRegisterActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/tancolle/UserRegisterActivity.java
@@ -1,50 +1,97 @@
 package org.t_robop.y_ogawara.tancolle;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.util.Calendar;
+import java.util.StringTokenizer;
 
 public class UserRegisterActivity extends AppCompatActivity {
+    private static final int REQUEST_GALLERY = 0;//ギャラリー選択で必要な初期化
 
     EditText edit_name;
     EditText edit_pho;
     EditText edit_twitter;
     EditText edit_days_ago;
 
+    ImageView user_view;
+
+    TextView user_birthday;
+
     InputMethodManager inputMethodManager;
+
+
+    DatePickerDialog datePickerDialog;
+//    int rema;
+//    int nowM;
+//    int nowD;
+    Calendar calendar;
+    Calendar nowCale;
+    Calendar birthCale;
+
+    int y;
+    int m;
+    int d;
+
+    int birthday;
 
 
 
     //sppinerTest
-    private Spinner tamura;
-    private String spinnerTamuraItems[] = {"田村", "ゆかりん", "40歳"};
-    private TextView TamuraView;
+    private Spinner spinnerCategory;
 
     private Spinner repetition;
     private String spinnerRepetitionItems[] = {"繰り返し通知無し", "毎日", "一週間毎","一ヶ月毎"};
 
+
+    private AlertDialog Adddialog;
+
+    //EditText用変数群
+    View viewV;
+    LayoutInflater inflater;
+    EditText editText;
+
+    ArrayAdapter<String> adapter;
+
+
+
+
+    /////////////////////////Override/////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
 
         //数多（予定）の宣言
-        TamuraView = (TextView)findViewById(R.id.tamura);
-        tamura = (Spinner)findViewById(R.id.spinner1);
         repetition=(Spinner)findViewById(R.id.spinner2);
 
         //Spinner設定
-        sppinerDraw(tamura,TamuraView,spinnerTamuraItems);
+        sppinerCategory();
         sppinerSet(repetition,spinnerRepetitionItems);
 
         edit_name=(EditText)findViewById(R.id.EditName);
@@ -74,8 +121,70 @@ public class UserRegisterActivity extends AppCompatActivity {
         EditTextSet(edit_twitter);
         EditTextSet(edit_days_ago);
 
+        //ImageViewの初期設定
+        user_view= (ImageView)findViewById(R.id.userview);
+        user_view.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        user_view.setImageResource(R.drawable.kaede);
+
+        user_birthday=(TextView)findViewById(R.id.userbirthday);
+
+
+        //誕生日初期設定
+        BirthTodaySet();
+
+        //誕生日描画
+        BirthDayDraw(birthday);
+
+
+
+        inflater = LayoutInflater.from(UserRegisterActivity.this);
+        viewV = inflater.inflate(R.layout.dialog_user_register, null);
+        editText = (EditText)viewV.findViewById(R.id.editText1);
 
     }
+
+    //画像をドキュメントから選択からのImageViewセット
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //TODO Auto-generated method stub
+        if(requestCode == REQUEST_GALLERY && resultCode == RESULT_OK) {
+            try {
+                InputStream in = getContentResolver().openInputStream(data.getData());
+                Bitmap pct = BitmapFactory.decodeStream(in);
+
+                // 作られたサムネイルから横幅とそれに応じた高さを指定
+                int pctWidth = pct.getWidth();
+                int pctHeight = pct.getHeight();
+
+                //1000以上であれば5で割らせる
+                if(pctWidth>1000||pctHeight>1000) {
+                    pctWidth = pctWidth / 3;
+                    pctHeight = pctHeight / 3;
+                }
+                else//1000以下であれば2で許す
+                {
+                    pctWidth = pctWidth / 2;
+                    pctHeight = pctHeight / 2;
+                }
+
+                // bitmapの画像を元の比率から最適化された比率で作成する
+                Bitmap img = Bitmap.createScaledBitmap(pct, pctWidth, pctHeight, false);
+                in.close();
+
+                // 選択した画像を表示
+                user_view.setImageBitmap(img);
+                } catch (Exception e) {
+
+                }
+            }
+        }
+
+    ////////////////////////////////////////////////////////////
+
+
+
+
+    ////////////////////クリック処理群/////////////////////////
 
     public void EditName(View v)
     {
@@ -109,6 +218,30 @@ public class UserRegisterActivity extends AppCompatActivity {
         inputMethodManager.showSoftInput(v, InputMethodManager.SHOW_FORCED);
     }
 
+    //画像クリック時
+    public void UserView(View v)
+    {
+        // ギャラリー呼び出し
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, REQUEST_GALLERY);
+    }
+
+    //誕生日クリック時
+    public void BirthDay(View v)
+    {
+        DatePickerSet();
+        // 日付設定ダイアログの表示
+        datePickerDialog.show();
+    }
+
+    //////////////////////////////////////////////////
+
+
+
+
+    ////////////////////自作関数群////////////////////
 
     public void EditDaysAgo()
     {
@@ -178,36 +311,69 @@ public class UserRegisterActivity extends AppCompatActivity {
     }
 
 
-    //spinnerとTextView使おうぜ！！（セットするspinner,セットするTextView,セットするString）
-    public void sppinerDraw(Spinner nSpinner, final TextView textView, final String spinnerItems[])
+    //カテゴリー追加用spinner
+    public void sppinerCategory()
     {
-        // ArrayAdapter
-        ArrayAdapter<String> adapter
-                = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerItems);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // spinner に adapter をセット
-        nSpinner.setAdapter(adapter);
+        // アイテムを追加します
+        adapter.add("<選択>");
+        adapter.add("+");
+        spinnerCategory = (Spinner) findViewById(R.id.spinner1);
+        // アダプターを設定します
+        spinnerCategory.setAdapter(adapter);
+        // スピナーのアイテムが選択された時に呼び出されるコールバックリスナーを登録します
+        spinnerCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                //処理
 
-        // リスナーを登録
-        nSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            //　アイテムが選択された時
-            public void onItemSelected(AdapterView<?> parent, View viw, int arg2, long arg3) {
-                Spinner spinner = (Spinner) parent;
-                String item = (String) spinner.getSelectedItem();
-
-                if (item.equals(spinnerItems[1])) {
-                    textView.setText(spinnerItems[1]);
-                } else if (item.equals(spinnerItems[2])) {
-                    textView.setText(spinnerItems[2]);
-                } else {
-                    textView.setText(spinnerItems[0]);
+                if(position==1)
+                {
+                    SpinnerCategoryAdd();
+                    Adddialog.show();
                 }
-            }
+                else
+                {
+                    //ここでカテゴリの名前保存しよう
+                }
 
-            //　アイテムが選択されなかった
-            public void onNothingSelected(AdapterView<?> parent) {
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
             }
         });
+
+
+//        // ArrayAdapter
+//        ArrayAdapter<String> adapter
+//                = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerItems);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        // spinner に adapter をセット
+//        nSpinner.setAdapter(adapter);
+//
+//        // リスナーを登録
+//        nSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            //　アイテムが選択された時
+//            public void onItemSelected(AdapterView<?> parent, View viw, int arg2, long arg3) {
+//                Spinner spinner = (Spinner) parent;
+//                String item = (String) spinner.getSelectedItem();
+//
+//                if (item.equals(spinnerItems[1])) {
+//
+//                } else if (item.equals(spinnerItems[2])) {
+//
+//                } else {
+//
+//                }
+//            }
+//
+//            //　アイテムが選択されなかった
+//            public void onNothingSelected(AdapterView<?> parent) {
+//            }
+//        });
     }
 
     //spinner使おうぜ！！（セットするspinner,セットするTextView,セットするString）
@@ -249,5 +415,135 @@ public class UserRegisterActivity extends AppCompatActivity {
             }
         });
     }
+
+    //現在の日付の取得
+    public void BirthTodaySet()
+    {
+        ////////// 日付情報の初期設定 //////////
+        calendar = Calendar.getInstance();
+
+        y = calendar.get(Calendar.YEAR); // 年
+        m = calendar.get(Calendar.MONTH); // 月
+        d = calendar.get(Calendar.DAY_OF_MONTH); // 日
+
+        nowCale=calendar;
+
+        birthday=BirthDayGet(y,m,d);
+
+        ////////////////////////////////////////
+    }
+
+    //DatePickerの設定
+    public void DatePickerSet()
+    {
+        // 日付設定時のリスナ作成
+        //ok押した時の処理
+        DatePickerDialog.OnDateSetListener DateSetListener = new DatePickerDialog.OnDateSetListener() {
+            //okボタンを押したときの年月日を取得できる
+            public void onDateSet(DatePicker datePicker, int year,
+                                  int monthOfYear, int dayOfMonth) {
+                //押されてる日時を変数へ
+                y = year;
+                m = monthOfYear;
+                d = dayOfMonth;
+
+                //birthCale.set(y,m,d);
+
+                birthday=BirthDayGet(y,m,d);
+
+                //描画
+                BirthDayDraw(birthday);
+            }
+        };
+        // 日付設定ダイアログの作成・リスナの登録
+        datePickerDialog = new DatePickerDialog(this, DateSetListener, y, m, d);
+    }
+
+
+    //カレンダーデータを八桁の数値へ
+    public int BirthDayGet(int Year,int Month,int Day)
+    {
+        int caly,calm;
+
+        caly=Year*10000;
+        calm=Month*100;
+
+        return caly+calm+Day;
+    }
+
+    //八桁の数値から算出してTextViewに描画
+    public void BirthDayDraw(int Birthday)
+    {
+        int caly,calm,cald,cal;
+
+        caly=Birthday/10000;
+
+        calm=Birthday/100;
+        cal=caly*100;
+        calm=calm-cal;
+
+        cal=caly*10000;
+        cald=Birthday-cal;
+        cal=calm*100;
+        cald=cald-cal;
+
+        y=caly;
+        m=calm;
+        d=cald;
+
+        cal=m+1;
+
+        user_birthday.setText(y+"/"+cal+"/"+d);
+    }
+
+    //spinnerCategory追加処理
+    public void SpinnerCategoryAdd()
+    {
+        if(Adddialog == null)//Adddialogが作成されていない時
+        {
+
+
+            Adddialog = new AlertDialog.Builder(UserRegisterActivity.this)
+                    .setTitle("カテゴリー入力")//DiaLogタイトル
+                    .setView(viewV)//View指定
+                    //DiaLog内の決定を押した時の処理
+                    .setPositiveButton("決定", new DialogInterface.OnClickListener() //ボタン作成
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //クリック時の処理
+
+                            //入力値取得用変数
+                            String addcategory;
+
+                            // エディットテキストのテキストを全選択します
+                            editText.selectAll();
+
+                            //editTextに何も入力されてない時
+                            if (editText.getText().toString().equals("")) {
+                                addcategory = "";//0が入る
+                            } else {
+                                // エディットテキストのテキストを取得します
+                                addcategory = editText.getText().toString();
+                            }
+
+                            //要素追加
+                            //リストとadaptorに入力値を入れる
+                            adapter.add(addcategory);
+
+                            //adapter更新
+                            adapter.notifyDataSetChanged();
+
+                            editText.getEditableText().clear();//editTextの初期化
+
+                            spinnerCategory.setSelection(adapter.getPosition(addcategory));
+
+                        }
+                    })
+                    .create();//初回AddDiaLog制作
+        }
+    }
+
+    //////////////////////////////////////////////////
 
 }
