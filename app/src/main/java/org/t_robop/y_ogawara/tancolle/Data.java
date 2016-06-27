65d7213b936d2ccd37f0fe42cce935100a3fe361
y-ogawara <yuusukeogawara@gmail.com>
2016-06-27 13:19:00 +0900

    Databaseの仕様変更

--- app/src/main/java/org/t_robop/y_ogawara/tancolle/Data.java
+++ app/src/main/java/org/t_robop/y_ogawara/tancolle/Data.java
@@ -17,6 +17,12 @@ public class Data {
     private String kana;
     //誕生日
     private int birthday;
+    //年
+    private int year;
+    //月
+    private int month;
+    //日
+    private int day;
     //カテゴリ
     private String category;
     //twitterのID
@@ -165,6 +171,31 @@ public class Data {
     public void setNotif_day(int notif_day) {
         this.notif_day = notif_day;
     }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
 
 
     void error(Context context){
