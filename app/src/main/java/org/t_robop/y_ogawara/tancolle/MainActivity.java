750ebf5f319b149c20287a6d00d67485843d4597
y-ogawara <yuusukeogawara@gmail.com>
2016-06-29 14:25:20 +0900

    mainActivityを各画面に飛べるように修正

--- app/src/main/java/org/t_robop/y_ogawara/tancolle/MainActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/tancolle/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
         FloatingActionButton add = (FloatingActionButton) findViewById(R.id.add);
