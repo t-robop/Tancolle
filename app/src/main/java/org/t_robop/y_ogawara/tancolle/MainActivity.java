c98401da755bc15a19f521217769fe4c7b02b695
taiga <6bji2119@mail.u-tokai.ac.jp>
2016-07-08 21:01:34 +0900

    メイン画面のデータ格納するやつ終わりました

--- app/src/main/java/org/t_robop/y_ogawara/tancolle/MainActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/tancolle/MainActivity.java
@@ -40,15 +40,15 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-        ArrayList<String> str = new ArrayList<>();
-        str.add("aaa");
-        str.add("bbb");
-        str.add("ccc");
-        str.add("abv");
-        saveArray(str,"test");
-        String[] aaa = getArray1("test");
-
-        Log.d(",",",");
+//        ArrayList<String> str = new ArrayList<>();
+//        str.add("aaa");
+//        str.add("bbb");
+//        str.add("ccc");
+//        str.add("abv");
+//        saveArray(str,"test");
+//        String[] aaa = getArray1("test");
+//
+//        Log.d(",",",");
 
 
         //adapterDataセット
@@ -64,25 +64,28 @@ public class MainActivity extends AppCompatActivity {
 
         Mad.startMad();//クラスの変数の初期化
 
+        int num=monthTurnData.size();//int型変数numにmonthTurnDataの配列数を入れる
+
         //読み込んだ月のデータの数だけ回す。（3分の1でいいのと、後述のListデータの取得に使うため+3）
-        for(int j=0;j<monthTurnData.size();j=j+3)//TODO .size()-3で回避できるので初期の三つのどこかの設定がおかしいのかも
+        for(int j=0;j<monthTurnData.size();j=j+3)
         {
             //三人分だけ保存するため3回回す。
             for (int i = 0; i < 3; i++) {
                 Data getData;//monthTurnData取得用のデータ型
 
-                //TODO monthTurnData.get(i+j)で落ちます
-
                 Log.d("aaaa", String.valueOf(i+j));
-                Log.d("aaaa", String.valueOf(monthTurnData.get(i+j)));
 
-                getData=monthTurnData.get(i+j);//読み込んだListの要素を取得
+                if(i<num)//iとnumを比較してiの方が低い時だけ（データ無いのに取得しようとして落ちるやつの修正）
+                {
+                    getData = monthTurnData.get(i + j);//読み込んだListの要素を取得
 
-                Mad.setId(i, getData.getId());//idのセット
-                Mad.setName(i, getData.getName());//名前のセット
-                Mad.setBirthMonth(i, getData.getMonth());//誕生月のセット
-                Mad.setBirthDay(i, getData.getDay());//誕生日のセット
-                Mad.setPresentFlag(i, getData.isPresentFlag());//プレゼントフラグのセット
+                    Mad.setId(i, getData.getId());//idのセット
+                    Mad.setName(i, getData.getName());//名前のセット
+                    Mad.setBirthMonth(i, getData.getMonth());//誕生月のセット
+                    Mad.setBirthDay(i, getData.getDay());//誕生日のセット
+                    Mad.setPresentFlag(i, getData.isPresentFlag());//プレゼントフラグのセット
+                }
+                num=num-3;//ここでnumから今セットした三人分だけ引く
             }
 
             //この辺に書き込み処理書いてくらさい。
@@ -123,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
                 testData.setKana("にしむら");
                 testData.setBirthday(19970616);
                 testData.setYear(1997);
-                testData.setMonth(06);
+                testData.setMonth(7);
                 testData.setDay(16);
                 testData.setCategory("友達");
                 testData.setTwitterID("Taiga_Natto");
@@ -146,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
                 testData1.setKana("にら");
                 testData1.setBirthday(19970512);
                 testData1.setYear(1997);
-                testData1.setMonth(05);
+                testData1.setMonth(5);
                 testData1.setDay(12);
                 testData1.setCategory("友達");
                 testData1.setTwitterID("Taiga_Natvaevto");
