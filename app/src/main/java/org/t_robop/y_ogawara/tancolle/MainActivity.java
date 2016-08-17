bcba393430905dbf81aab5e6fdf9fbce41736b94
y-ogawara <yuusukeogawara@gmail.com>
2016-08-17 15:34:15 +0900

    dbに新しいデータを追加したので、各自アプリを削除して再インストールしてください。

--- app/src/main/java/org/t_robop/y_ogawara/tancolle/MainActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/tancolle/MainActivity.java
@@ -13,7 +13,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     public static final int MENU_SELECT_A = 0;
@@ -52,28 +55,27 @@ public class MainActivity extends AppCompatActivity {
 
 
         //adapterDataセット
-        int month=7;//とりあえず7月でプレイ(ここらへんで月の指定お願いします)
+        int month = 7;//とりあえず7月でプレイ(ここらへんで月の指定お願いします)
 
         ArrayList<Data> monthTurnData;//ArrayListの宣言
 
-        monthTurnData=dbAssist.birthdaySelect(month,this);//ArrayListに月検索したデータをぶち込む
+        monthTurnData = dbAssist.birthdaySelect(month, this);//ArrayListに月検索したデータをぶち込む
 
         MainAdapterData Mad = new MainAdapterData();//自分で作成したclassの宣言
 
-        ArrayList<MainAdapterData> adapterData=new ArrayList<>();//classのArrayListの作成
+        ArrayList<MainAdapterData> adapterData = new ArrayList<>();//classのArrayListの作成
 
         //Mad.startMad();//クラスの変数の初期化
 
-        int num=monthTurnData.size();//int型変数numにmonthTurnDataの配列数を入れる
+        int num = monthTurnData.size();//int型変数numにmonthTurnDataの配列数を入れる
 
         //読み込んだ月のデータの数だけ回す。（3分の1でいいのと、後述のListデータの取得に使うため+3）
-        for(int j=0;j<monthTurnData.size();j=j+3)
-        {
+        for (int j = 0; j < monthTurnData.size(); j = j + 3) {
             //三人分だけ保存するため3回回す。
             for (int i = 0; i < 3; i++) {
                 Data getData;//monthTurnData取得用のデータ型
 
-                if(i<num)//iとnumを比較してiの方が低い時だけ（データ無いのに取得しようとして落ちるやつの修正）
+                if (i < num)//iとnumを比較してiの方が低い時だけ（データ無いのに取得しようとして落ちるやつの修正）
                 {
                     getData = monthTurnData.get(i + j);//読み込んだListの要素を取得
 
@@ -83,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                     Mad.setBirthDay(i, getData.getDay());//誕生日のセット
                     Mad.setPresentFlag(i, getData.isPresentFlag());//プレゼントフラグのセット
                 }
-                num=num-1;//ここでnumから今セットした三人分だけ引く
+                num = num - 1;//ここでnumから今セットした三人分だけ引く
             }
 
             //この辺に書き込み処理書いてくらさい。
@@ -116,9 +118,9 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         switch (item.getItemId()) {
             case MENU_SELECT_A:
-                Log.d("Menu","SQliteに書き込み");
+                Log.d("Menu", "SQliteに書き込み");
                 //Data型の宣言
-                Data testData =new Data();
+                Data testData = new Data();
                 //Data型にデータをセット
                 testData.setName("西村");
                 testData.setKana("にしむら");
@@ -138,10 +140,10 @@ public class MainActivity extends AppCompatActivity {
                 testData.setNotif_month(3);
                 testData.setNotif_week(3);
                 //dbに書き込み
-                dbAssist.insertData(testData,this);
+                dbAssist.insertData(testData, this);
 
                 //Data型の宣言
-                Data testData1 =new Data();
+                Data testData1 = new Data();
                 //Data型にデータをセット
                 testData1.setName("西");
                 testData1.setKana("にら");
@@ -162,51 +164,51 @@ public class MainActivity extends AppCompatActivity {
                 testData1.setNotif_week(31);
                 //dbに書き込み
                 //引数はData,場所(this)
-                dbAssist.insertData(testData1,this);
+                dbAssist.insertData(testData1, this);
                 return true;
 
             case MENU_SELECT_B:
-                Log.d("Menu","DBをアップデートします");
-                Data updateData =new Data();
+                Log.d("Menu", "DBをアップデートします");
+                Data updateData = new Data();
                 updateData.setName("aaaaa");
                 updateData.setBirthday(19970714);
                 updateData.setNotif_yest(1);
                 updateData.setNotif_week(0);
                 //引数はid,data,場所
-                dbAssist.updateData(1,updateData,this);
+                dbAssist.updateData(1, updateData, this);
                 return true;
 
             case MENU_SELECT_C:
-                Log.d("Menu","全データ検索");
+                Log.d("Menu", "全データ検索");
                 //引数は場所
                 //返り値は配列
-                ArrayList<Data> dataList=  dbAssist.allSelect(this);
-                Data allData =new Data();
+                ArrayList<Data> dataList = dbAssist.allSelect(this);
+                Data allData = new Data();
                 allData = dataList.get(1);
 
                 Log.d("aaaaaaaaaaaa", dataList.get(1).getKana());
                 return true;
 
             case MENU_SELECT_D:
-                Log.d("Menu","仮名検索");
+                Log.d("Menu", "仮名検索");
                 //引数はkana(String),場所
                 //返り値は配列
-                ArrayList<Data> kanaList=  dbAssist.kanaSelect("にしむら",this);
-                Log.d("","");
+                ArrayList<Data> kanaList = dbAssist.kanaSelect("にしむら", this);
+                Log.d("", "");
                 return true;
 
             case MENU_SELECT_E:
-                Log.d("Menu","id検索");
+                Log.d("Menu", "id検索");
                 //引数はid(int),場所
                 //idは固有値なので、返り値はData型
-                Data idList=  dbAssist.idSelect(1,this);
-                Log.d("","");
+                Data idList = dbAssist.idSelect(1, this);
+                Log.d("", "");
                 return true;
 
             case MENU_SELECT_F:
-                Log.d("Menu","削除");
+                Log.d("Menu", "削除");
                 //引数はid,場所
-                dbAssist.deleteData(1,this);
+                dbAssist.deleteData(1, this);
                 return true;
         }
         return false;
@@ -214,24 +216,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void UserRegisterActivityBtn(View view) {
-        Intent intent = new Intent(this,UserRegisterActivity.class);
+        Intent intent = new Intent(this, UserRegisterActivity.class);
         startActivity(intent);
     }
+
     public void UserDetailActivityBtn(View view) {
-        Intent intent = new Intent(this,UserDetailActivity.class);
+        Intent intent = new Intent(this, UserDetailActivity.class);
         startActivity(intent);
 
     }
+
     public void SearchActivityBtn(View view) {
-        Intent intent = new Intent(this,SearchActivity.class);
+        Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
     }
 
-    private void saveArray(ArrayList<String> array, String PrefKey){
+    private void saveArray(ArrayList<String> array, String PrefKey) {
         String str = "";
-        for (int i =0;i<array.size();i++){
+        for (int i = 0; i < array.size(); i++) {
             str = str + array.get(i);
-            if (i !=array.size()-1){
+            if (i != array.size() - 1) {
                 str = str + ",";
             }
         }
@@ -239,24 +243,48 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = prefs1.edit();
         editor.putString(PrefKey, str).commit();
     }
+
     // aaa,bbb,ccc...としたものをsplitして返す
-    private String[] getArray1(String PrefKey){
+    private String[] getArray1(String PrefKey) {
         SharedPreferences prefs2 = getSharedPreferences("Array", Context.MODE_PRIVATE);
-        String stringItem = prefs2.getString(PrefKey,"");
-        if(stringItem != null && stringItem.length() != 0){
+        String stringItem = prefs2.getString(PrefKey, "");
+        if (stringItem != null && stringItem.length() != 0) {
             return stringItem.split(",");
-        }else{
+        } else {
             return null;
         }
     }
 
     public void ViewFlipperTestActivityBtn(View view) {
-        Intent intent = new Intent(this,ViewFlipperTestActivity.class);
+        Intent intent = new Intent(this, ViewFlipperTestActivity.class);
         startActivity(intent);
     }
 
     public void HomeActivityBtn(View view) {
-        Intent intent = new Intent(this,GestureDecActivity.class);
+        Intent intent = new Intent(this, GestureDecActivity.class);
         startActivity(intent);
+    }
+
+    public long conversion(int year, int month, int day) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        Date dateTo = null;
+        Date dateFrom = null;
+
+
+        try {
+            dateTo = sdf.parse(year + "/" + month + "/" + day); //誕生日ができる
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        //現在の日時取得をココらへんに書いてね！！
+
+        //ここでDateFromに入れて！
+        //dateFrom =
+
+        long dateToTime = dateTo.getTime();
+        long dateFromTime = dateFrom.getTime();
+
+        return dateToTime - dateFromTime;
     }
 }
