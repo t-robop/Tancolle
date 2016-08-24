1c6cc3b74e678bf688f2208bc73c4e2dfd198a2c
multiverse2011 <multiverse2011@gmail.com>
2016-08-24 20:08:52 +0900

    SearchActivityでonResumeを使うように

--- app/src/main/java/org/t_robop/y_ogawara/tancolle/SearchActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/tancolle/SearchActivity.java
@@ -88,6 +88,59 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
 
     }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        listView = (ListView) findViewById(R.id.listView);
+        searchView = (SearchView) findViewById(R.id.searchView);
+
+        ArrayList<Data> dataList = dbAssist.allSelect(this);
+
+
+        ArrayList<ListItem> name = new ArrayList<>();
+        for (int i = 0; dataList.size() > i; i++) {
+            ListItem item = new ListItem();
+            item.setName(dataList.get(i).getName());
+            item.setKana(dataList.get(i).getKana());
+            item.setItemId(dataList.get(i).getId());
+            item.setSmallImage(dataList.get(i).getSmallImage());
+            Log.d("aaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaa");
+            Log.d("search", dataList.get(i).getImage());
+            name.add(item);
+        }
+        //adapterをセット
+        adapter = new dataListAdapter(getApplicationContext(), name);
+        listView.setAdapter(adapter);
+
+        //searchViewに関する設定
+        searchView.setOnQueryTextListener(this);
+        searchView.setQueryHint("名前を入力");
+        searchView.setIconifiedByDefault(false);
+
+        // ListView がクリックされた時に呼び出されるコールバックを登録
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                //ここに書く
+                //listViewのitemを取得してadapterからItemをもらってくる
+                ListView listView = (ListView) parent;
+                listView.getItemAtPosition(position);
+
+                ListItem listItem = (ListItem) listView.getAdapter().getItem(position);
+
+                //Intentで飛ばす＆idをキーにする
+                Intent intent = new Intent(SearchActivity.this, UserDetailActivity.class);
+                intent.putExtra("id", listItem.getItemId());
+                startActivity(intent);
+
+                //トースト
+                //Toast.makeText(SearchActivity.this, "Click: " + item, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
     //Textが変わった時に呼び出される
     @Override
     public boolean onQueryTextChange(String newText) {
