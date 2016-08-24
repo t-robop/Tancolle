68adef5e92cc9d3770b1383a9d0e3ef7b63f9e87
y-ogawara <yuusukeogawara@gmail.com>
2016-08-25 02:42:19 +0900

    switchをbreakしました。

--- app/src/main/java/org/t_robop/y_ogawara/tancolle/UserRegisterActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/tancolle/UserRegisterActivity.java
@@ -573,6 +573,7 @@ public class UserRegisterActivity extends AppCompatActivity implements TextWatch
                     AllRegist();
                     return true;
                 }
+                break;
             //toolbarの戻るボタン
             case android.R.id.home:
                 //編集されてた時
@@ -589,6 +590,7 @@ public class UserRegisterActivity extends AppCompatActivity implements TextWatch
             default:
                 return super.onOptionsItemSelected(item);
         }
+        return super.onOptionsItemSelected(item);
     }
 
 
